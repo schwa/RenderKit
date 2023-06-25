de9a4729e426393148780894931cfbd0f95b09cf
Jonathan Wight <jwight@mac.com>
2023-06-25 12:44:36 -0700

    Added empty package & demos.

---
+++ Demos/Demos/DemosApp.swift
@@ -0,0 +1,17 @@
+//
+//  DemosApp.swift
+//  Demos
+//
+//  Created by Jonathan Wight on 6/25/23.
+//
+
+import SwiftUI
+
+@main
+struct DemosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
