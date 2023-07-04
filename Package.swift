711af148ed63801ad38fb45333bff7b2ba9e3093
Jonathan Wight <jwight@mac.com>
2023-07-04 16:13:09 -0700

    Housekeeping.

--- Package.swift
+++ Package.swift
@@ -14,15 +14,16 @@ let package = Package(
             targets: ["RenderKit"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/schwa/Everything", from: "0.2.0"),
+//        .package(url: "https://github.com/schwa/Everything", from: "0.4.3"),
+//        .package(url: "https://github.com/schwa/Everything", branch: "main"),
+        .package(path: "/Users/schwa/Projects/Everything"),
         .package(url: "https://github.com/schwa/MetalSupport", from: "0.2.0"),
-        //.package(url: "https://github.com/schwa/RenderKit", branch: "main"),
         .package(url: "https://github.com/schwa/SIMD-Support", from: "0.1.1"),
         .package(url: "https://github.com/schwa/CoreGraphicsGeometrySupport", from: "0.1.0"),
         .package(url: "https://github.com/apple/swift-algorithms", from: "1.0.0"),
         .package(url: "https://github.com/apple/swift-async-algorithms", from: "0.1.0"),
-        .package(url: "https://github.com/schwa/swiftformats", from: "0.3.0"),
-        .package(url: "https://github.com/schwa/swiftfields", from: "0.1.0"),
+        .package(url: "https://github.com/schwa/swiftformats", from: "0.3.3"),
+        .package(url: "https://github.com/schwa/swiftfields", from: "0.1.3"),
     ],
     targets: [
         .target(
@@ -33,7 +34,6 @@ let package = Package(
                 "Shaders",
                 .product(name: "MetalSupport", package: "MetalSupport"),
                 .product(name: "MetalSupportUnsafeConformances", package: "MetalSupport"),
-                //.product(name: "RenderKitSupport", package: "RenderKit"),
                 .product(name: "SIMDSupport", package: "SIMD-Support"),
                 .product(name: "Algorithms", package: "swift-algorithms"),
                 .product(name: "SwiftFields", package: "swiftfields"),
