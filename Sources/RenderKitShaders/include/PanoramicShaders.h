16b2f4cff786fdea38eba253c1133cc3e0a79bbf
Jonathan Wight <jwight@mac.com>
2023-10-24 16:59:50 -0700

    WIP

--- Sources/RenderKitShaders/include/PanoramicShaders.h
+++ Sources/RenderKitShaders/include/PanoramicShaders.h
@@ -1,6 +1,21 @@
 #import "RenderKitShaders.h"
+#import "CommonTypes.h"
 
 struct PanoramaFragmentUniforms {
     simd_ushort2 gridSize;
     float4 colorFactor;
+};
+
+struct PanoramaShader {
+    simd_ushort2 gridSize;
+    float4 colorFactor;
+
+    struct CameraUniforms camera;
+    struct ModelTransforms modelTransforms;
+
+#ifdef __METAL_VERSION__
+    device texture2d<float> *textures;
+#else
+    void *textures;
+#endif
 };
